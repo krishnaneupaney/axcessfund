6fab8d2ad7185310d68b4e3df8373b47635d8e40
Krishna <krishna@Krishnas-MBP.attlocal.net>
2021-10-17 17:26:30 -0400

    contact us

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -52,7 +52,7 @@ function Navbar() {
                         </li>
                         <li className='nav-item'>
                             <Link to='/contact' className='nav-links' onClick={closeMobileMenu} onSubmit={ContactForm}>
-                                Contact
+                                Contact Us
                             </Link>
 
                         </li>
