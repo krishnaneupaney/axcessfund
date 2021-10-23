872ab8d41ba36922bf5b50f3cf04b213baf16a42
Krishna <krishna@Krishnas-MBP.attlocal.net>
2021-10-23 13:39:49 -0400

    google sign in works

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import ContactForm from './components/pages/Contact';
 import About from './components/pages/About';
 import Footer from './components/pages/Footer';
 import React from 'react';
-import { Container } from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
@@ -13,11 +12,8 @@ function App() {
 
 
   return (
-
-
     <BrowserRouter>
       <Navbar />
-
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/auth" exact component={Auth} />
