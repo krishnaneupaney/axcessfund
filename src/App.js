ec55cbcc450edcbceaa3e6758a871e9a19ac361d
Krishna <krishna@Krishnas-MBP.attlocal.net>
2021-10-23 17:54:16 -0400

    google works

--- src/App.js
+++ src/App.js
@@ -7,21 +7,24 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Navbar from './components/Navbar';
 import Auth from './components/Auth/Auth';
-
+import { Container } from '@material-ui/core';
 function App() {
 
 
   return (
     <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/auth" exact component={Auth} />
-        <Route path="/about" exact component={About} />
-        <Route path="/contact" exact component={ContactForm} />
+      <Container maxWidth="lg">
 
-      </Switch>
-      <Footer />
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+          <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={ContactForm} />
+
+        </Switch>
+        <Footer />
+      </Container>
     </BrowserRouter>
   )
 }
