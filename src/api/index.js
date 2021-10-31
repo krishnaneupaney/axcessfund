9a83a1c7aea3cbf7c9454299d1adce410d0080d1
Krishna <krishna@Krishnas-MBP.attlocal.net>
2021-10-31 00:24:24 -0400

    made

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://axcessfund.com/posts' });
+const API = axios.create({ baseURL: 'https://axcessfund.com/posts' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
