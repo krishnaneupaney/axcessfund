5b077e4fe2032ccfc358b433ab5d422cefdb095f
Krishna <krishna@Krishnas-MBP.attlocal.net>
2021-10-31 00:41:24 -0400

    I guess

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://axcessfund.com/posts' });
+const API = axios.create({ baseURL: 'http://localhost:3000' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
