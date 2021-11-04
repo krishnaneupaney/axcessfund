36bb9bdc00aaa4c926802faf99368dae0ea2f3e3
Krishna <krishna@Krishnas-MBP.attlocal.net>
2021-11-04 17:36:55 -0400

    yeas

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://axcess-back.herokuapp.com/posts' });
+const API = axios.create({ baseURL: 'https://axcess-back.herokuapp.com' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
